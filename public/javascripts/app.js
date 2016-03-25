c34854188a617011d72c98d92993bf9e9fecf0bf
brennenrampton <rampton.brennen@gmail.com>
2016-03-25 17:51:50 -0600

    fixed typo

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,10 +4,10 @@ angular.module('music', [])
 	function($scope){
 		$scope.test = 'Hello world';
 		$scope.songs = [
-      		{title:'The Boxer', album:'Greatest Hits', artist:'Simon and Garfunkel', Genre:'Pop', imageURL:'https://upload.wikimedia.org/wikipedia/en/c/c6/Sggreatesthits.jpg', upvotes:7}
+      		{title:'The Boxer', album:'Greatest Hits', artist:'Simon and Garfunkel', genre:'Pop', imageURL:'https://upload.wikimedia.org/wikipedia/en/c/c6/Sggreatesthits.jpg', upvotes:7}
     	];
     	$scope.addSong = function() {
-      		$scope.songs.push({title:$scope.formTitle, album:$scope.formAlbum, artist:$scope.formArtist, Genre:$scope.formGenre, imageURL:$scope.formURL, upvotes:0});
+      		$scope.songs.push({title:$scope.formTitle, album:$scope.formAlbum, artist:$scope.formArtist, genre:$scope.formGenre, imageURL:$scope.formURL, upvotes:0});
       		$scope.formTitle='';
           $scope.formAlbum='';
           $scope.formArtist='';
